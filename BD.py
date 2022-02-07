2ffb6e18f4c6a09fee731cf4f8e955dfa3098df3
johan-valero <valero_johan@hotmail.fr>
2022-02-07 09:13:08 +0100

    delete

--- BD.py
+++
@@ -1,6 +0,0 @@
-from Livre import *
-
-class BD(Livre):
-    def __init__(self,titre, auteur, langue,
-                 genre, categorie, ref, dispo):
-        super().__init__(titre, auteur, langue, genre, categorie, ref, dispo )
