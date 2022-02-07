544ae80ceb7025153c3595823601b951ee5c5843
AceTBan <94051289+AceTBan@users.noreply.github.com>
2022-02-07 09:55:46 +0100

    commit du lundi matin -O-
    modification des fichiers user & personne.py

--- Personne.py
+++ Personne.py
@@ -1,8 +1,6 @@
 class Personne:
-    def __init__(self, nom, prenom, mdp):
-        self.nom = nom
-        self.prenom = prenom
-        self.mdp = mdp
-        
-    def __repr__(self):
-        return self.nom
+  def __init__(self, nom, prenom, mdp):
+    self.id = nom[0] + "." + prenom[0]
+    self.nom = nom
+    self.prenom = prenom
+    self.mdp = mdp
--- User.py
+++ User.py
@@ -1,9 +1,13 @@
-from Personne import *
-
+from Personne import Personne
 
 class User(Personne):
+  def __init__(self, nom, prenom, mdp, emprunts, grade):
+    super().__init__(nom, prenom, mdp)
+    self.emprunts = emprunts
+    self.grade = grade
+  
+  def __repr__(self):
+    return str(f"[{self.nom}, {self.prenom}, {self.mdp}, {self.emprunts}, {self.grade}]")
 
-    def __init__(self, Id,  nom, prenom, mdp, emprunts, grade):
-        super().__init__(Id,  nom, prenom, mdp)
-        self.emprunts = emprunts
-        self.grade = grade
+  def AfficherEmprunts(self):
+    print(self.emprunts)
