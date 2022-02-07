a113ef9a7955c96ea8020c3923551ec8b3a13f5c
AceTBan <94051289+AceTBan@users.noreply.github.com>
2022-02-07 12:29:31 +0100

    modif user.py

--- User.py
+++ User.py
@@ -1,4 +1,7 @@
 from Personne import Personne
+from Livre import *
+from Bibliotheque import *
+from datatime import date, timedelta
 
 class User(Personne):
   def __init__(self, nom, prenom, mdp, emprunts, grade):
@@ -11,3 +14,26 @@ class User(Personne):
 
   def AfficherEmprunts(self):
     print(self.emprunts)
+
+  def EmprunterLivre(self, user, livre):
+    dateDuJour = date.today()
+    tempsEmprunt = timedelta(days=7)
+    dateRetour = dateDuJour + tempsEmprunt
+
+    livre.dispo = False
+    livre.retour = dateRetour
+
+    user.emprunts.append(livre)
+  
+  def RendreLivre(self, user, livre):
+    livre.dispo = True
+    livre.retour = None
+    indexLivre = user.emprunts.index(livre)
+
+    del user.emprunts[indexLivre]
+
+# def Grade (self):
+#     grades = {"moldu":[0,1],"sangdebourge":[2,5],"sorcier":[6,10],"auror":[11,40]}
+
+# emprunts = 0
+
