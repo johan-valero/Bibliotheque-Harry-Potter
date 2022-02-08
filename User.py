3ea0a114419c9d894989374eb222e38ab8337c94
AceTBan <94051289+AceTBan@users.noreply.github.com>
2022-02-08 15:50:35 +0100

    add def Prolongation

--- User.py
+++ User.py
@@ -1,3 +1,4 @@
+import datetime
 from Personne import Personne
 from Livre import *
 from Bibliotheque import *
@@ -14,11 +15,11 @@ class User(Personne):
         return str(f"[{self.nom}, {self.prenom}, {self.mdp}, {self.emprunts}, {self.grade}]")
 
     def AfficherEmprunts(self, bibliotheque):
-      print("Vous avez", len(self.emprunts), "emprunt(s) en cours :")
-      j = 0
-      for i in self.emprunts:
-        j += 1
-        print(j, "-", i.titre, "à rendre avant le", bibliotheque.livre_liste[bibliotheque.RechercheIndex(i)].retour)
+        print("Vous avez", len(self.emprunts), "emprunt(s) en cours :")
+        j = 0
+        for i in self.emprunts:
+            j += 1
+            print(j, "-", i.titre, "à rendre avant le", bibliotheque.livre_liste[bibliotheque.RechercheIndex(i)].retour)
 
     def EmprunterLivre(self, bibliotheque, livre):
         dateDuJour = date.today()
@@ -48,4 +49,12 @@ class User(Personne):
             self.grade = "gollum"
 
     def ChangerMdp(self, new_mdp):
-      self.mdp = new_mdp
+        self.mdp = new_mdp
+
+    def Prolongation(self, bibliotheque, livre):
+        durée = input("Désirez-vous prolonger la durée d'emprunt du livre ? : (y/n) \n\t ")
+        if durée == "y":
+            dateRetour =+ timedelta(days=7)
+            bibliotheque.livre_liste[bibliotheque.RechercheIndex(livre)].retour = dateRetour
+        else:
+            pass
