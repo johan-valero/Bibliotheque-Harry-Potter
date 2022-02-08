de95b88d418e2a1e0121ca053a6e85a0367e1fcf
johan-valero <valero_johan@hotmail.fr>
2022-02-08 17:47:12 +0100

    update import export

--- Personne.py
+++ Personne.py
@@ -1,10 +1,10 @@
 class Personne:
     def __init__(self, nom, prenom, mdp):
-        self.id = nom[0] + "." + prenom[0]
+        self.id = nom[0] + "." + prenom
         self.nom = nom
         self.prenom = prenom
         self.mdp = mdp
     
     def IdUser(nom, prenom):
-        Id = str(nom[0]+"."+prenom)
+        Id = str(nom[0] + "." + prenom)
         return Id
