07a1d9ef53dc2619179313b2f96c5447cf675b0c
ted <teddu12@hotmail.fr>
2022-02-24 11:45:34 +0100

    Mise au propre TODO.py

--- TODO.py
+++ TODO.py
@@ -1,58 +1,83 @@
                                                     # ============== TODO LIST ==============
 
-# TODO 1: Les recherches de livres ne donnent pas droit à l’erreur (Pas de faute ou d’oublies de       majuscules). 
-# TODO 2: La prolongation d’un emprunt est limité uniquement à 7 jours et se fait sans limitation (On peut prolonger à volonté).
-# TODO 3: Les datas pourraient être mieux affichées dans le Livres.txt.
-# TODO 4: Meilleure organisation dans l’affichage global des livres (En filtrant nos choix par disponibilité par exemple).
-# TODO 5: Mieux factoriser le main.
-# TODO 6: L’utilisateur ne peut pas voir son nombre de points ou son grade. 
-# TODO 7: Le nom de l’utilisateur n’est pas affiché lorsqu'il est connecté. 
-# TODO 8: Ajout de getter, setter dans les classes
-# TODO 9: La méthode référence de livre pas encore utilisé 
-# TODO 10: Ordonnés les méthodes 
-# TODO 11: Améliorer les REPR (de bibliothèque, user) 
-# TODO 12: Ajout de str sur les classes 
+
+# Admin
+# TODO 01: Créer Classe
+# TODO 02: Ajout Méthodes
+# TODO 03: Accès au menu admin
+# TODO 04: La méthode référence de livre pas encore utilisé 
 
 # Recherche ========================================================================================================
 
-# TODO 13: Revoir affichage (Ajouter filtres )
-# TODO 14: Mettre en cachet l’affichage par référence 
-# TODO 15: Revoir affichage et recherche livre
-# TODO 16: Problème de retour 
-# TODO 17: Problème de sur le strict des recherches (Regex, Str in ,chaîne de caractère, utilisation du    Lowercase())
-# TODO 18: Cacher affichage et recherche par réf du user (accès que pour l’admin)
+# TODO 05: Revoir affichage (Ajouter filtres )
+# TODO 06: Cacher l’affichage par référence pour les Users 
+# TODO 07: Revoir affichage et recherche livre
+# TODO 08: Problème de retour 1 pour RecherchParRef et liste pour les autres
+# TODO 09: Cacher affichage et recherche par réf du user (accès que pour l’admin)
 
-# Input ========================================================================================================
+# Gestion Globale ========================================================================================================
 
-# TODO 19: Cacher le mot de passe à la saisie 
-# TODO 20: Ajouter double check sur mot pour la création de compte
-# TODO 21: Ajouter Gitignore
-# TODO 22: Revoir import des fichiers pour enlever redondance fonction clear et check# TODO 2:in
-# TODO 23: Ajouter déconnexion si changement de mot de passe 3 fois d’affiler
-# TODO 24: Compteur tentative sur rechecking mot de passe
-# TODO 25: Changer ordre attribut (Pour les réservation)pour classes Livres et BD pour précision future  avec admin : enregistrer livres étant pas disponible.
-# TODO 26: Correction de l’attribut retour de livre et BD (qui n’est pas Date Time)
-# TODO 27: Changer type du retour
-# TODO 28: Suppression de f.close (sur les with open)
-# TODO 29: Normalisation et optimisation du code 
-# TODO 30: Problème de sécurité sur l'authentification (un hackeur connaît l’existence d’un pseudo)
-# TODO 31: Ajouter les réservations  
-# TODO 32: Ajout de commentaire pour plus de clarté 
-# TODO 33: Supprimer commentaire inutile
+# TODO 10: Ajouter Gitignore
+# TODO 11: Revoir import des fichiers pour enlever redondance fonction clear et check# TODO 2:in
 
 # Générale ========================================================================================================
 
-# TODO 34: Transfert des ASCII sur les fonctions
-# TODO 35: Transfert menu 2 : 2 # TODO 1: Emprunter dans classes la classe User pour réduire le main
-# TODO 36: Mettre inscription User dans le User
-# TODO 37: Ajouter fonction start / quitter pour vider le main 
-# TODO 38: Changer le nom de bibliothèque (Poney_fringant ne fais pas partie de Harry Wesh)
-# TODO 39: Revoir REPR et les unifier (nom de l’attribut: Valeur de l’attribut )
-# TODO 40: Rajout de Str pour remplacer afficher 
-# TODO 41: Rechercher par Titre doit être revu en retirant la liste 
-# TODO 42: Modifier Catégorie et genre
-# TODO 43: Connexion direct après création de compte 
-# TODO 44: Mettre les points des maisons dans un dico 
-# TODO 45: Revoir affichage résultat recherche 
-# TODO 46: Afficher les livres lors des emprunts (dispo en vert , non dispo en rouge)
-# TODO 47: Améliorer affichage du château au début du main 
+
+# display
+# TODO 12: Transfert des ASCII sur les fonctions
+# TODO 13: Améliorer affichage du château au début du main (ajout couleur, revoir trait en-dessous, l'enjoliver) 
+# TODO 14: Revoir affichage résultat recherche 
+
+# Corrections 
+    # Erreurs
+    # TODO 15: Modifier Catégorie et genre
+    # ///TODO 16: Changer le nom de bibliothèque (Poney_fringant ne fais pas partie de Harry Wesh)
+    # ///TODO 17: Suppression de f.close (sur les with open)
+    # TODO 18: Correction de l’attribut .retour de livre et BD (qui n’est pas Date Time) sur l'import / Export
+    # TODO 19: Corriger ProlongerEmprunt l'input prend 0
+
+    # Logique
+    # TODO 20: La prolongation d’un emprunt est limité uniquement à 7 jours et se fait sans limitation (On peut prolonger à volonté).
+
+# Optimisation 
+    # main
+    # TODO 21: Ajouter fonction start / quitter pour vider le main 
+    # TODO 22: Transfert menu 2 : 2 # TODO 1: Emprunter dans classes la classe User pour réduire le main
+    # TODO 23: Mettre inscription User dans le User
+
+    # Classes
+    # TODO 24: Mettre les points des maisons dans un dico 
+    # TODO 25: Revoir REPR et les unifier (nom de l’attribut: Valeur de l’attribut )
+    # TODO 26: Rajout de Str pour remplacer afficher 
+    # TODO 27: Utilisation des str des classes pour affichage (simple)
+    # TODO 28: Ajout de getter, setter dans les classes (très lourd à mettre en place)
+    # TODO 29: Les recherches de livres ne donnent pas droit à l’erreur (Pas de faute ou d’oublies de majuscules). 
+
+# Ergonomie
+# TODO 30: Rechercher par Titre doit être revu en ajoutant code couleur pour distinguer dispo ou non 
+# TODO 31: Connexion direct après création de compte 
+# TODO 32: Afficher les livres lors des emprunts (dispo en vert , non dispo en rouge)
+# TODO 33: L’utilisateur ne peut pas voir son nombre de points ou son grade. 
+# TODO 34: Le nom de l’utilisateur n’est pas affiché lorsqu'il est connecté. 
+
+# Aide à la lecture du code
+# TODO 35: Ajout de commentaire pour plus de clarté 
+# TODO 36: Supprimer commentaire inutile
+# TODO 37: Normalisation et optimisation du code 
+# TODO 38: Ordonner les méthodes 
+
+# Amélioration Sécurité
+# TODO 39: Ajouter déconnexion si changement de mot de passe 3 fois d’affiler
+# TODO 40: Compteur tentative sur rechecking mot de passe
+# TODO 41: Problème de sécurité sur l'authentification (un hackeur connaît l’existence d’un pseudo)
+# TODO 42: Ajouter double check sur mot pour la création de compte
+# TODO 43: Cacher le mot de passe à la saisie 
+# TODO 44: Problème de sur le strict des recherches (Regex, Str in ,chaîne de caractère, utilisation du    Lowercase())
+
+# Features
+# TODO 45: Changer ordre attribut (Pour les réservation)pour classes Livres et BD pour précision future  avec admin : enregistrer livres étant pas disponible.
+# TODO 46: Ajouter les réservations  
+
+# Ajouter Classes pour mise en place Médiathèque (pas trop top dans un monde de magie au pire leur emprunt induit un malus xD)
+# TODO 47: Classe CD
+# TODO 48: Classe DVD
