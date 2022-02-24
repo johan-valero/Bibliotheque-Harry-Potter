4224434b70da4187bd1c5fee13e4fece8dd3140c
AceTBan <esteban_david@ymail.com>
2022-02-24 14:16:09 +0100

    TODO 15: Modifier Catégorie et genre

--- TODO.py
+++ TODO.py
@@ -30,7 +30,7 @@
 
 # Corrections 
     # Erreurs
-    # TODO 15: Modifier Catégorie et genre
+    # ///TODO 15: Modifier Catégorie et genre
     # ///TODO 16: Changer le nom de bibliothèque (Poney_fringant ne fais pas partie de Harry Wesh)
     # ///TODO 17: Suppression de f.close (sur les with open)
     # TODO 18: Correction de l’attribut .retour de livre et BD (qui n’est pas Date Time) sur l'import / Export
